2ee343a8c739c5d40708f4b7a6401b45e225bed4
catlam <130130218+catlam@users.noreply.github.com>
2024-09-11 22:58:54 +0700

    Generate OpenAPI Spec from JSDoc

--- src/routes/contacts.router.js
+++ src/routes/contacts.router.js
@@ -7,13 +7,14 @@ const router = express.Router();
 
 module.exports.setup = (app) => {
     app.use('/api/v1/contacts', router);
+
     /**
      * @swagger
      * /api/v1/contacts:
-     * get:
-     *  summary: Get contacts by filter
-     *  description: Get contacts by filter
-     *  parameters:
+     *  get:
+     *    summary: Get contacts by filter
+     *    description: Get contacts by filter
+     *    parameters:
      *      - in: query
      *        name: favorite
      *        schema:
@@ -24,37 +25,129 @@ module.exports.setup = (app) => {
      *        schema:
      *          type: string 
      *        description: Get contacts contact name
-     *  tags:
-     *      -contacts
-     *  response:
-     *     200:
-     *      description: A list of contacts
-     *      content:
+     *    tags:
+     *      - contacts
+     *    responses:
+     *      200:
+     *        description: A list of contacts
+     *        content:
      *          application/json:
-     *              schema:
+     *            schema:
+     *              type: object
+     *              properties:
+     *                status:
+     *                  type: string
+     *                  description: The response status
+     *                  enum: [success]
+     *                data:
      *                  type: object
      *                  properties:
-     *                      status:
-     *                          type: string
-     *                          description: The response status
-     *                          enum: [success]
-     *                      data:
-     *                          type: object
-     *                          properties:
-     *                              contacts:
-     *                                  type: array
-     *                                  items:
-     *                                      $ref: '#/components/schemas/Contact'
-     * 
+     *                    contacts:
+     *                      type: array
+     *                      items:
+     *                        $ref: '#/components/schemas/Contact'
     */
 
     router.get('/', contactsController.getContactsByFilter);
     router.post('/', contactsController.createContact);
+
+    /**
+     * @swagger
+     * /api/v1/contacts:
+     *   delete:
+     *     summary: Delete all contacts
+     *     description: Delete all contacts
+     *     tags:
+     *       - contacts
+     *     responses:
+     *       200:
+     *         description:: All contacts deleted
+     *         $ref: '#/components/responses/200NoData'
+     */
     router.delete('/', contactsController.deleteAllContacts);
     router.all('/', methodNotAllowed);
 
+    /**
+     * @swagger
+     * /api/v1/contacts/{id}:
+     *  get:
+     *    summary: Get contacts by ID
+     *    description: Get contacts by ID
+     *    parameters:
+     *      - $ref: '#/components/parameters/contactIdParam'
+     *    tags:
+     *      - contacts
+     *    responses:
+     *      200:
+     *        description: A contact
+     *        content:
+     *          application/json:
+     *            schema:
+     *              type: object
+     *              properties:
+     *                status:
+     *                  type: string
+     *                  description: The response status
+     *                  enum: [success]
+     *                data:
+     *                  type: object
+     *                  properties:
+     *                    contacts:
+     *                      $ref: '#/components/schemas/Contact'
+    */
     router.get('/:id', contactsController.getContact);
+
+    /**
+     * @swagger
+     * /api/v1/contacts/{id}:
+     *  put:
+     *    summary: Update contacts by ID
+     *    description: Get contacts by ID
+     *    parameters:
+     *      - $ref: '#/components/parameters/contactIdParam'
+     *    requestBody:
+     *      required: true
+     *      content:
+     *        multipart/form-data:
+     *          schema:
+     *            $ref: '#/components/schemas/Contact'
+     *    tags:
+     *      - contacts
+     *    responses:
+     *      200:
+     *        description: A update contact
+     *        content:
+     *          application/json:
+     *            schema:
+     *              type: object
+     *              properties:
+     *                status:
+     *                  type: string
+     *                  description: The response status
+     *                  enum: [success]
+     *                data:
+     *                  type: object
+     *                  properties:
+     *                    contacts:
+     *                      $ref: '#/components/schemas/Contact'
+    */
     router.put('/:id', contactsController.updateContact);
+
+    /**
+     * @swagger
+     * /api/v1/contacts/{id}:
+     *   delete:
+     *     summary: Delete contact by ID
+     *     description: Delete contact by ID
+     *     parameters:
+     *       - $ref: '#/components/parameters/ContactIdParam'
+     *     tags:
+     *       - contacts
+     *     responses:
+     *       200:
+     *         description:: Contact deleted 
+     *         $ref: '#/components/responses/200NoData'
+     */
     router.delete('/:id', contactsController.deleteContact);
     router.all('/:id', methodNotAllowed);
 }
