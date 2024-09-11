0097b3de0da84c8ef362ac3097e9c41bbde069ca
catlam <130130218+catlam@users.noreply.github.com>
2024-09-11 12:05:10 +0700

    Define routes for managing contacts

--- src/controllers/contacts.controller.js
+++ src/controllers/contacts.controller.js
@@ -1,7 +1,8 @@
+/* global require */
 const JSend = require('../jsend');
 
 function createContact(req, res) {
-    return res.status(201).json({ contact: {} })
+    return res.status(201).json(JSend.success({ contact: {} }));
 }
 
 function getContactsByFilter(req, res) {
@@ -17,27 +18,27 @@ function getContactsByFilter(req, res) {
 
     console.log(filters.join('&'));
 
-    return res.json({ contacts: [] });
+    return res.json(
+        JSend.success({
+            contacts: []
+        })
+    );
 }
 
 function getContact(req, res) {
-    return res.json({ contacts: {} });
+    return res.json(JSend.success({ contact: {} }));
 }
 
 function updateContact(req, res) {
-    return res.json({ contact: {} });
+    return res.json(JSend.success({ contact: {} }));
 }
 
 function deleteContact(req, res) {
-    return res.json({
-        message: 'Contact deleted'
-    })
+    return res.json(JSend.success());
 }
 
 function deleteAllContacts(req, res) {
-    return res.json({
-        message: 'All contacts deleted'
-    })
+    return res.json(JSend.success());
 }
 
 module.exports = {
