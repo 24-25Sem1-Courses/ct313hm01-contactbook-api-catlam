bc992862733f93d603eac87556a44dcae4062b3c
catlam <130130218+catlam@users.noreply.github.com>
2024-09-11 08:48:44 +0700

    Setup express server and show an ok message

---
+++ src/app.js
@@ -0,0 +1,17 @@
+/*global module, require */
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    return res.json({
+        message: 'ok'
+    });
+});
+
+module.exports = app;
